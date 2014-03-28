bed37d122f2a2d639a8e78c1cb384461bc7a78d1
Raphael Morand <raphael.morand@epitech.eu>
2014-03-28 10:17:39 +0100

    recode Snake (comming: fruits)

---
+++ src/Snake.cpp
@@ -0,0 +1,120 @@
+//
+// Snake.cpp for  in /home/hallelouia/Tek2/C++/nibbler-loc
+// 
+// Made by Raphael Morand
+// Login   <morand_c@epitech.net>
+// 
+// Started on  Wed Mar 26 14:56:48 2014 Raphael Morand
+// Last update Thu Mar 27 17:07:44 2014 Raphael Morand
+//
+
+#include		"Snake.hh"
+
+Snake::Snake(const std::pair<int, int>& _xyMap)
+{
+  int		i = 1;
+
+  _size = 4;
+  _dir = UP;
+  _snake.push_back(std::pair<int, int>(_xyMap.first / 2, _xyMap.second / 2));
+  while (i < _size)
+    {
+      _snake.push_back(std::pair<int, int>(_snake[i - 1].first, _snake[i - 1].second + 1));
+      ++i;
+    }
+  _head = _snake[0];
+  _tail = _snake[3];
+  move_fct[0] = &Snake::move_up;
+  move_fct[1] = &Snake::move_down;
+  move_fct[2] = &Snake::move_right;
+  move_fct[3] = &Snake::move_left;
+}
+
+Snake::~Snake()
+{}
+
+const std::vector<std::pair<int, int> >&	Snake::getSnake() const
+{return (_snake);}
+
+const int		Snake::getSize() const
+{return (_size);}
+
+const bool		Snake::checkCollision(const std::pair<int, int>& map) const
+{
+  int		i = 1;
+
+  if (_head.first == 0 || _head.first == map.first + 1)
+    return (true);
+  if (_head.second == 0 || _head.second == map.second + 1)
+    return (true);
+  while (i != _size)
+    {
+      if (_head == _snake[i])
+	return (true);
+      ++i;
+    }
+  return (false);
+}		
+
+void			Snake::addSnake()
+{
+  _snake.push_back(_tail);
+  ++_size;
+}
+
+void			Snake::resetTail()
+{_tail = _snake.back();}
+
+void			Snake::move_up()
+{
+  int			i = 1;
+
+  while (i < _size)
+    {
+      _snake[i] = _snake[i - 1];
+      ++i;
+    }
+  _snake[0].second--;
+}
+
+void			Snake::move_down()
+{
+  int			i = 1;
+
+  while (i < _size)
+    {
+      _snake[i] = _snake[i - 1];
+      ++i;
+    }
+  _snake[0].second++;
+}
+
+void			Snake::move_right()
+{
+  int			i = 1;
+
+  while (i < _size)
+    {
+      _snake[i] = _snake[i - 1];
+      ++i;
+    }
+  _snake[0].first++;
+}
+
+void			Snake::move_left()
+{
+  int			i = 1;
+
+  while (i < _size)
+    {
+      _snake[i] = _snake[i - 1];
+      ++i;
+    }
+  _snake[0].first--;
+}
+
+void			Snake::moveSnake()
+{(this->*move_fct[_dir])();}
+
+void			Snake::setDir(const t_directions dir)
+{_dir = dir;}
